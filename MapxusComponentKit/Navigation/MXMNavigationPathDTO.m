7882ec7db9185a1559a5c2b7d52e74b2b23aff36
guochenghao <chenghaoguo@mapxus.com>
2023-12-13 11:03:01 +0800

    Update format

--- MapxusComponentKit/Navigation/MXMNavigationPathDTO.m
+++ MapxusComponentKit/Navigation/MXMNavigationPathDTO.m
@@ -12,61 +12,61 @@
 @implementation MXMNavigationPathDTO
 
 - (instancetype)initWithPath:(MXMPath *)path {
-    self = [super init];
-    if (self) {
-      NSMutableDictionary *floorIdMap = [NSMutableDictionary dictionary];
-      floorIdMap[@"outdoor"] = @"outdoor";
-        NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-        NSArray *instructions = path.instructions;
-        NSArray *points = path.points.coordinates;
-        int i = 0;
-        for (MXMInstruction *instruction in instructions) {
-            // 小节的楼层标签
-          NSString *key = [MXMNavigationPathDTO generateKeyUsingBuildingId:instruction.buildingId andFloor:instruction.floor];
-          if (instruction.floorId) {
-            floorIdMap[instruction.floorId] = key;
-          }
-            
-          // 加上前后两段的原因是因为楼梯线段只属于某一层楼，在上电梯时容易因为定位精度问题吸附不了，所以在两层楼的端点都加上电梯段来预防
-            /// 前一段
-            LineArray *list1;
-            if (i-1 >= 0 && i-1 < instructions.count) {
-                MXMInstruction *preInstruction = instructions[i-1];
-                /// 是梯子转换段
-                if (preInstruction.sign == MXMDownstairs || preInstruction.sign == MXMUpstairs) {
-                    list1 = [self breakUpLineSegmentWithInstruction:preInstruction index:i-1 points:points];
-                }
-            }
-            /// 当前遍历段
-            LineArray *list2 = [self breakUpLineSegmentWithInstruction:instruction index:i points:points];
-            /// 后一段
-            LineArray *list3;
-            if (i+1 >= 0 && i+1 < instructions.count) {
-                MXMInstruction *nextInstruction = instructions[i+1];
-                if (nextInstruction.sign == MXMDownstairs || nextInstruction.sign == MXMUpstairs) {
-                    list3 = [self breakUpLineSegmentWithInstruction:nextInstruction index:i+1 points:points];
-                }
-            }
-            NSMutableArray *oldList = dictionary[key];
-            NSMutableArray *newList = [NSMutableArray array];
-            if (oldList) {
-                [newList addObjectsFromArray:oldList];
-                [newList addObjectsFromArray:list1];
-                [newList addObjectsFromArray:list2];
-                [newList addObjectsFromArray:list3];
-            } else {
-                [newList addObjectsFromArray:list1];
-                [newList addObjectsFromArray:list2];
-                [newList addObjectsFromArray:list3];
-            }
-            dictionary[key] = [newList copy];
-            
-            i++;
+  self = [super init];
+  if (self) {
+    NSMutableDictionary *floorIdMap = [NSMutableDictionary dictionary];
+    floorIdMap[@"outdoor"] = @"outdoor";
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    NSArray *instructions = path.instructions;
+    NSArray *points = path.points.coordinates;
+    int i = 0;
+    for (MXMInstruction *instruction in instructions) {
+      // 小节的楼层标签
+      NSString *key = [MXMNavigationPathDTO generateKeyUsingBuildingId:instruction.buildingId andFloor:instruction.floor];
+      if (instruction.floorId) {
+        floorIdMap[instruction.floorId] = key;
+      }
+      
+      // 加上前后两段的原因是因为楼梯线段只属于某一层楼，在上电梯时容易因为定位精度问题吸附不了，所以在两层楼的端点都加上电梯段来预防
+      /// 前一段
+      LineArray *list1;
+      if (i-1 >= 0 && i-1 < instructions.count) {
+        MXMInstruction *preInstruction = instructions[i-1];
+        /// 是梯子转换段
+        if (preInstruction.sign == MXMDownstairs || preInstruction.sign == MXMUpstairs) {
+          list1 = [self breakUpLineSegmentWithInstruction:preInstruction index:i-1 points:points];
         }
-        _fragments = [dictionary copy];
-      _floorIdMap = [floorIdMap copy];
+      }
+      /// 当前遍历段
+      LineArray *list2 = [self breakUpLineSegmentWithInstruction:instruction index:i points:points];
+      /// 后一段
+      LineArray *list3;
+      if (i+1 >= 0 && i+1 < instructions.count) {
+        MXMInstruction *nextInstruction = instructions[i+1];
+        if (nextInstruction.sign == MXMDownstairs || nextInstruction.sign == MXMUpstairs) {
+          list3 = [self breakUpLineSegmentWithInstruction:nextInstruction index:i+1 points:points];
+        }
+      }
+      NSMutableArray *oldList = dictionary[key];
+      NSMutableArray *newList = [NSMutableArray array];
+      if (oldList) {
+        [newList addObjectsFromArray:oldList];
+        [newList addObjectsFromArray:list1];
+        [newList addObjectsFromArray:list2];
+        [newList addObjectsFromArray:list3];
+      } else {
+        [newList addObjectsFromArray:list1];
+        [newList addObjectsFromArray:list2];
+        [newList addObjectsFromArray:list3];
+      }
+      dictionary[key] = [newList copy];
+      
+      i++;
     }
-    return self;
+    _fragments = [dictionary copy];
+    _floorIdMap = [floorIdMap copy];
+  }
+  return self;
 }
 
 - (LineArray *)breakUpLineSegmentWithInstruction:(MXMInstruction *)instruction index:(NSUInteger)index points:(NSArray<MXMGeoPoint *> *)points {
@@ -112,16 +112,16 @@
 }
 
 - (nullable LineArray *)fragmenntWithKey:(NSString *)key {
-    return self.fragments[key];
+  return self.fragments[key];
 }
 
 + (NSString *)generateKeyUsingBuildingId:(nullable NSString *)buildingId andFloor:(nullable NSString *)floor {
-    NSString *key;
-    if (buildingId != nil && floor != nil && buildingId.length != 0 && floor.length != 0) {
-        key = [NSString stringWithFormat:@"%@-%@", buildingId, floor];
-    } else {
-        key = @"outdoor";
-    }
-    return key;
+  NSString *key;
+  if (buildingId != nil && floor != nil && buildingId.length != 0 && floor.length != 0) {
+    key = [NSString stringWithFormat:@"%@-%@", buildingId, floor];
+  } else {
+    key = @"outdoor";
+  }
+  return key;
 }
 @end
