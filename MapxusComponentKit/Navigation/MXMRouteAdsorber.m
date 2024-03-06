9ed4d2aa56d29dc4ea148451ebd97736a59621cd
guochenghao <chenghaoguo@mapxus.com>
2024-03-06 11:12:06 +0800

    [SDK-924] 在导航过程中，如果在坐电梯等情况下导致室内定位失效时，重绘路线时会错误地绘制出一条室外线段

--- MapxusComponentKit/Navigation/MXMRouteAdsorber.m
+++ MapxusComponentKit/Navigation/MXMRouteAdsorber.m
@@ -11,16 +11,25 @@
 #import "MXMRouteProjection.h"
 #import "MXMLoggerService.h"
 
-struct ProjectResult {
-    MXMAdsorptionState state;
-    CLLocation *location;
-};
+@interface ProjectResult : NSObject
+@property (nonatomic, assign) MXMAdsorptionState state;
+@property (nonatomic, strong, nullable) CLLocation *location;
+@property (nonatomic, strong, nullable) NSString *venueId;
+@property (nonatomic, strong, nullable) NSString *buildingId;
+@property (nonatomic, strong, nullable) NSString *floorId;
+@property (nonatomic, strong, nullable) NSString *floorCode;
+@end
+
+@implementation ProjectResult
+@end
+
+
 
 @interface MXMRouteAdsorber ()
 
 @property (nonatomic, strong) MXMNavigationPathDTO *pathDTO;
 @property (nonatomic, assign) NSUInteger count;
-@property (nonatomic, strong, nullable) CLLocation *previousLocation;
+@property (nonatomic, strong, nullable) ProjectResult *previousResult;
 @property (nonatomic, strong) dispatch_queue_t subQueue;
 
 @end
@@ -39,7 +48,7 @@ struct ProjectResult {
 
 - (void)updateNavigationPathDTO:(MXMNavigationPathDTO *)navigationPathDTO {
     self.count = 0;
-    self.previousLocation = nil;
+    self.previousResult = nil;
     self.pathDTO = navigationPathDTO;
 }
 
@@ -62,49 +71,75 @@ struct ProjectResult {
         floorId = result.floor.floorId;
         floor = result.floor.code;
       }
-      // TODO: 返回吸附数据，当刚开始找不到吸附点时，会返回上一次的吸附结果，但使用的却是当前定位解析的floorId，这会造成在绘制路线时画出一条乱飞的线段。
       NSString *key = [MXMNavigationPathDTO generateKeyUsingBuildingId:buildingId andFloor:floor];
-      struct ProjectResult final = [self calculateNewLocationWithCurrent:actual key:key];
+      ProjectResult *final = [self calculateNewLocationWithCurrent:actual key:key venueId:venueId buildingId:buildingId floorId:floorId floorCode:floor];
       dispatch_async(dispatch_get_main_queue(), ^{
         if (self.delegate && [self.delegate respondsToSelector:@selector(refreshTheAdsorptionLocation:venueId:buildingId:floorId:state:fromActual:)]) {
           [self.delegate refreshTheAdsorptionLocation:final.location
-                                              venueId:venueId
-                                           buildingId:buildingId
-                                              floorId:floorId
+                                              venueId:final.venueId
+                                           buildingId:final.buildingId
+                                              floorId:final.floorId
                                                 state:final.state
                                            fromActual:actual];
         } else if (self.delegate && [self.delegate respondsToSelector:@selector(refreshTheAdsorptionLocation:buildingID:floor:state:fromActual:)]) {
-          [self.delegate refreshTheAdsorptionLocation:final.location buildingID:buildingId floor:floor state:final.state fromActual:actual];
+          [self.delegate refreshTheAdsorptionLocation:final.location 
+                                           buildingID:final.buildingId
+                                                floor:final.floorCode
+                                                state:final.state
+                                           fromActual:actual];
         }
       });
     }];
   });
 }
 
-- (struct ProjectResult)calculateNewLocationWithCurrent:(CLLocation *)current key:(NSString *)key {
-    struct ProjectResult result;
+- (ProjectResult *)calculateNewLocationWithCurrent:(CLLocation *)current
+                                                    key:(NSString *)key
+                                                venueId:(NSString *)venueId
+                                             buildingId:(NSString *)buildingId 
+                                                floorId:(NSString *)floorId
+                                              floorCode:(NSString *)floorCode {
+    ProjectResult *result = [[ProjectResult alloc] init];
     CLLocation *projectLocation = [MXMRouteProjection getProjectedResultWithPathDTO:self.pathDTO userPosition:current key:key maximumDrift:self.maximumDrift];
     if (projectLocation) {
         self.count = 0;
-        self.previousLocation = projectLocation;
+      
         result.location = projectLocation;
         result.state = MXMAdsorptionStateDefault;
+        result.venueId = venueId;
+        result.buildingId = buildingId;
+        result.floorId = floorId;
+        result.floorCode = floorCode;
+      
+        self.previousResult = result;
         return result;
     } else {
         // 如果未开始吸附，则不会重新计算路线
-        if (self.previousLocation == nil) {
+        if (self.previousResult == nil) {
             result.location = current;
             result.state = MXMAdsorptionStateNotStartBinding;
+            result.venueId = venueId;
+            result.buildingId = buildingId;
+            result.floorId = floorId;
+            result.floorCode = floorCode;
             return result;
         }
         if (self.count > self.numberOfAllowedDrifts) {
             result.location = current;
             result.state = MXMAdsorptionStateDriftsNumberExceeded;
+            result.venueId = venueId;
+            result.buildingId = buildingId;
+            result.floorId = floorId;
+            result.floorCode = floorCode;
             return result;
         } else {
             self.count++;
-            result.location = self.previousLocation;
+            result.location = self.previousResult.location;
             result.state = MXMAdsorptionStateDrifting;
+            result.venueId = self.previousResult.venueId;
+            result.buildingId = self.previousResult.buildingId;
+            result.floorId = self.previousResult.floorId;
+            result.floorCode = self.previousResult.floorCode;
             return result;
         }
     }
