f85489910100a99c4542067489bd0b4b451b2b34
guochenghao <chenghaoguo@mapxus.com>
2025-03-21 10:12:00 +0800

    # CHANGELOG
    ## v7.4.0 (2025-03-21)
    
    ### 🎉 New
    
    * The route drawing supports the indoor 2.5D effect.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "7.3.0"
+let version = "7.4.0"
 
 let package = Package(
   name: "MapxusComponentKit",
@@ -19,7 +19,7 @@ let package = Package(
     .binaryTarget(
       name: "MapxusComponentKit",
       url: "https://nexus3.mapxus.com/repository/ios-sdk/\(version)/mapxus-component-kit-ios.zip",
-      checksum: "fea9c6628614e9f4a6ad35465b8566db3b979469bcf75eb66a7ab0a2b31baa34"
+      checksum: "7f0645e53cf373efd14e1d3462ebb4b98b1cc6db29720f44a942b319adb26131"
     )
   ]
 )
